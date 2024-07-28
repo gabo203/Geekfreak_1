06277dcbe644bbb9bed86bd5495296a81a72be07
gabo203 <ga.ibanez@duocuc.cl>
2024-07-27 23:17:04 -0400

    agregado mensaje de enviado

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,7 @@ document.getElementById('form')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Send Email';
-      alert('Sent!');
+      alert('Mensaje Enviado!');
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
